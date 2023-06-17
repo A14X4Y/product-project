52a06d40a2bfe6113839ce3f806d26b7f30eb132
A14X4Y <leshabelousov2001@gmail.com>
2023-06-18 01:06:41 +0500

    eslint refactor

--- src/shared/config/storybook/ThemeDecorator/ThemeDrcorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDrcorator.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/display-name */
 import { Theme } from 'app/providers/ThemeProvider';
 import { Story } from '@storybook/react';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) =>
   (
-    <div className={`app ${theme}`}>
+    <div className={`app ${theme}`}>S
       <StoryComponent />
     </div>
   );
