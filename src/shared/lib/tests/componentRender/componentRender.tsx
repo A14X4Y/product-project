c3849bcf84f86c315ad74bf35cdec7ccc10a6e2b
A14X4Y <leshabelousov2001@gmail.com>
2023-06-26 22:13:10 +0500

    fix: fix test sidebar

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import { i18n } from 'shared/config/i18n/i18ForTests';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+export interface RenderWithRouterOptions {
+  route?: string;
+}
+
+export function componentRender(component: ReactNode, options: RenderWithRouterOptions = {}) {
+  const { route = '/' } = options;
+  render(
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
+    </MemoryRouter>,
+  );
+}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,14 @@
 import { screen, fireEvent } from '@testing-library/react';
-import React from 'react';
-import { renderWithTranslation } from 'shared/lib/renderWithTransltaion/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { renderWithTranslation } from 'shared/lib/tests/renderWithTransltaion/renderWithTranslation';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 describe('Sidebar testing', () => {
   test('with only first param', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
